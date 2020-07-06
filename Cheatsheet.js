947f5f38c6a52d3e76fe50dce684fa0581636a79
aapostolou <63057992+aapostolou@users.noreply.github.com>
2020-07-06 11:16:45 +0300

    Update Cheatsheet.js

--- Cheatsheet.js
+++ Cheatsheet.js
@@ -79,6 +79,35 @@ Provider.childContextTypes = {
 	store: React.PropTypes.object
 }
 
+// connect()
+import { connect } from 'react-redux';
+const connect(mapStateToProps, mapDispatchToProps) => {
+  return function (WrappedComponent) {
+    return class extends React.Component {
+      render() {
+        return (
+          <WrappedComponent
+            {...this.props}
+            {...mapStateToProps(store.getState(), this.props)}
+            {...mapDispatchToProps(store.dispatch, this.props)}
+          />
+        )
+      }
+      componentDidMount() {
+        this.unsubscribe = store.subscribe(this.handleChange.bind(this))
+      }
+      
+      componentWillUnmount() {
+        this.unsubscribe()
+      }
+    
+      handleChange() {
+        this.forceUpdate()
+      }
+    } 
+  } 
+}
+
 // Init & Example
 const store = createStore(combineReducers({ counter, toggler }));
 
